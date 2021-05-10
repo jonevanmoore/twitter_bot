05be0896b46dca53bd47e79e1bb3370d93a8ee0a
Jon Evan Moore <64309316+jonevanmoore@users.noreply.github.com>
2021-05-10 15:12:48 -0600

    Create engaging_accounts.py

---
+++ engaging_accounts.py
@@ -0,0 +1,51 @@
+accounts = [
+#Jonathan Haidt
+        '“Morality binds and blinds. It binds us into ideological teams that fight each other as though the fate of the world depended on our side winning each battle. It blinds us to the fact that each team is composed of good people who have something important to say.” - @JonHaidt',
+        '“If you think that moral reasoning is something we do to figure out the truth, you’ll be constantly frustrated by how foolish, biased, and illogical people become when they disagree with you.” - @JonHaidt',
+        '“Anyone who values truth should stop worshipping reason.” - @JonHaidt',
+        '“Sports is to war as pornography is to sex.” - @JonHaidt']
+
+#Tim Ferriss
+        '“Information is useless if it is not applied to something important or if you will forget it before you have a chance to apply it.” - @tferriss',
+        '“To do the impossible, you need to ignore the popular.” - @tferriss',
+        '“If you are insecure, guess what? The rest of the world is, too. Do not overestimate the competition and underestimate yourself. You are better than you think.” - @tferriss',
+        '"You need to know what your unique strengths are and be willing to delegate." - @tferriss']
+
+
+
+
+
+
+
+
+#########
+#INDIVIDUAL ACCOUNTS for picture tweets
+#########
+
+naval = ['"The modern devil is cheap dopamine." - @naval',
+        '"The great viruses of our time spread through minds, not through bodies." - @naval',
+        '“Don’t do things that you know are morally wrong. Not because someone is watching, but because you are. Self-esteem is just the reputation that you have with yourself.” - @naval',
+        '“A rational person can find peace by cultivating indifference to things outside of their control.” - @naval']
+
+manson = ['"Who you are is defined by what you’re willing to struggle for." - @IAmMarkManson',
+        '"The desire for more positive experiences is itself a negative experience. And, paradoxically, the acceptance of one’s negative experience is itself a positive experience." - @IAmMarkManson',
+        '"It\'s easy to know what we hide from others. It\'s far more difficult to know what we hide from ourselves." - @IAmMarkManson',
+        '"The more something threatens your identity, the more you will avoid it." - @IAmMarkManson']
+
+haidt = ['“Morality binds and blinds. It binds us into ideological teams that fight each other as though the fate of the world depended on our side winning each battle. It blinds us to the fact that each team is composed of good people who have something important to say.” - @JonHaidt',
+        '“If you think that moral reasoning is something we do to figure out the truth, you’ll be constantly frustrated by how foolish, biased, and illogical people become when they disagree with you.” - @JonHaidt',
+        '“Anyone who values truth should stop worshipping reason.” - @JonHaidt']
+
+ferriss = ['“Information is useless if it is not applied to something important or if you will forget it before you have a chance to apply it.” - @tferriss',
+            '“To do the impossible, you need to ignore the popular.” - @tferriss',
+            '“If you are insecure, guess what? The rest of the world is, too. Do not overestimate the competition and underestimate yourself. You are better than you think.” - @tferriss']
+
+cain = ['"Introverts, in contrast, may have strong social skills and enjoy parties and business meetings, but after a while wish they were home in their pajamas. They prefer to devote their social energies to close friends, colleagues, and family." - @susancain',
+        '"[Introverts] listen more than they talk, think before they speak, and often feel as if they express themselves better in writing than in conversation. They tend to dislike conflict. Many have a horror of small talk, but enjoy deep discussions." - @susancain',
+        '"There\'s zero correlation between being the best talker and having the best ideas." - @susancain']
+
+mcgonigal = ['"The biggest enemies of willpower: temptation, self-criticism, and stress. [...] these three skills—self-awareness, self-care, and remembering what matter most—are the foundation for self-control." - @kellymcgonigal',
+            '"The is a secret for greater self-control, the science points to one thing: the power of paying attention." - @kellymcgonigal',
+            '"When your mind is preoccupied, your impulses—not your long-term goals—will guide your choices." - @kellymcgonigal',
+            '"A short practice that you do every day is better than a long practice you keep putting off to tomorrow." - @kellymcgonigal',
+            '"Research shows that people who think they have the most willpower are actually the most likely to lose control when tempted [...] Why? They fail to predict when, where, and why they will give in. They expose themselves to more temptation." - @kellymcgonigal']
