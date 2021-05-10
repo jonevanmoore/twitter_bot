99640fbd80b30221a02fb35e1844f1246badf9af
Jon Evan Moore <64309316+jonevanmoore@users.noreply.github.com>
2021-05-10 15:20:05 -0600

    Create affiliates.py

---
+++ affiliates.py
@@ -0,0 +1,65 @@
+affiliate_links =   {
+        #The Laws of Human Nature, Robert Greene
+                    #Hardcover
+                    "affiliate link goes here":
+                    "Expand your understanding of human nature. Get 'The Laws of Human Nature' by @RobertGreene.",
+                    #Audible
+                    "affiliate link goes here":
+                    "Expand your understanding of human nature. Get the Audible version of 'The Laws of Human Nature' by @RobertGreene.",
+                    #Short version
+                    "affiliate link goes here":
+                    "Expand your understanding of human nature. Get the condensed version of @RobertGreene's bestseller, 'Consise Laws of Human Nature'.",
+
+        #The Almanack of Naval Ravikant, Eric Jorgenson
+                    #Kindle
+                    "affiliate link goes here":
+                    "Learn the philosophy and wisdom of @naval. Get 'The Almanack of Naval Ravikant' by @EricJorgenson on Kindle for just $2.99, or download it for free by going to navalmanack.com.",
+                    #paperback
+                    "affiliate link goes here":
+                    "Learn the philosophy and wisdom of @naval. Get the paperback of 'The Almanack of Naval Ravikant' by @EricJorgenson, or download the pdf version for free by going to navalmanack.com.",
+                    #Audible
+                    "affiliate link goes here":
+                    "Learn the philosophy and wisdom of @naval. Get the Audible version of 'The Almanack of Naval Ravikant' by @EricJorgenson, or download the pdf version for free by going to navalmanack.com.",
+
+        #The True Believer, Eric Hoffer
+                    #paperback
+                    "affiliate link goes here":
+                    "Get the book that President Eisenhower cited during one of his first press conferenses: 'The True Believer' by #EricHoffer.",
+                    #Audible
+                    "affiliate link goes here":
+                    "Understand the nature of mass movements. Get the Audible version of 'The True Believer' by #EricHoffer.",
+
+        #Man's Search for Meaning, Viktor Frankl
+                    #paperback
+                    "affiliate link goes here":
+                    "Learn how #ViktorFrankl revolutionized the field of psychotherapy by enduring years of unspeakable horrors in Nazi death camps. Get 'Man's Search for Meaning'.",
+                    #Audible
+                    "affiliate link goes here":
+                    "Learn how #ViktorFrankl revolutionized the field of psychotherapy by enduring years of unspeakable horrors in Nazi death camps. Get the Audible version of 'Man's Search for Meaning'.",
+
+        #Surely, You're Joking Mr. Feynman, Richard Feynman
+                    #paperback
+                    "affiliate link goes here":
+                    "Dive into the personal life of one of the smartest people from the 20th century: '\"Surely You're Joking, Mr. Feynman!\"'.",
+
+        #Our Inner Ape, Frans de Waal
+                    #paperback
+                    "affiliate link goes here":
+                    "Have a better grasp of human nature by understanding the nature of our closest primate cousins, chimps and bonobos. Get 'Our Inner Ape' by #FransdeWaal.",
+        #Chimpanzee Politics, Frans de Waal
+                    #paperback
+                    "affiliate link goes here":
+                    "Learn about the political structures of chimpanzees to better understand how humans operate politically. Get 'Chimpanzee Politics' by #FransdeWaal.",
+        #The Bonobo and the Atheist, Frans de Waal
+                    #paperback
+                    "affiliate link goes here":
+                    "Understand how morality is innate in humans and not constructed by a social, religious or political group. Get 'The Bonobo and the Atheist' by #FransdeWaal.",
+
+        #Thinking, Fast and Slow, Daniel Kahneman
+                    "affiliate link goes here":
+                    "Learn how you process information, and how we shape our judgments and decision making. Get 'Thinking, Fast and Slow' by #DanielKahneman.",
+
+        #Quiet, Susan Cain
+                    "affiliate link goes here":
+                    "Understand the value of introversion and why we should embrace our introverted traits instead of stigmitizing them. Get 'Quiet' by @susancain."
+                    }
